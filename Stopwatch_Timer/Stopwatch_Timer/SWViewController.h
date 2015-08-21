519d17bc2ccb632f90062540466b980d3f9606ad
Jackie Meggesto <jackie.meggesto@gmail.com>
2015-08-21 13:43:37 -0400

    rudimentary framework for stopwatch

--- Stopwatch_Timer/Stopwatch_Timer/SWViewController.h
+++ Stopwatch_Timer/Stopwatch_Timer/SWViewController.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  SWViewController.h
 //  Stopwatch_Timer
 //
-//  Created by Varindra Hart on 8/21/15.
+//  Created by Jackie Meggesto on 8/21/15.
 //  Copyright (c) 2015 Varindra Hart. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface SWViewController : UIViewController
 
 @end
-
---
+++ Stopwatch_Timer/Stopwatch_Timer/SWViewController.m
@@ -0,0 +1,36 @@
+//
+//  SWViewController.m
+//  Stopwatch_Timer
+//
+//  Created by Jackie Meggesto on 8/21/15.
+//  Copyright (c) 2015 Varindra Hart. All rights reserved.
+//
+
+#import "SWViewController.h"
+#import <QuartzCore/QuartzCore.h>
+
+@interface SWViewController ()
+@property (nonatomic) CADisplayLink* stopwatchDisplayLink;
+
+@end
+
+@implementation SWViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+-(IBAction)startWatch:(id)sender {
+    
+}
+-(IBAction)stopWatch:(id)sender {
+    
+}
+
+
+
+
+
+@end
