61b900d70cbfdf6da8b42bc008053b7e59a47218
Varindra <varindrahart@gmail.com>
2015-08-22 10:24:04 -0400

    tableViewClass added

---
+++ Stopwatch_Timer/Stopwatch_Timer/SWTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  SWTableViewController.h
+//  Stopwatch_Timer
+//
+//  Created by Varindra Hart on 8/22/15.
+//  Copyright (c) 2015 Varindra Hart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWTableViewController : UITableViewController
+
+@property (nonatomic) NSMutableArray *lapTimesArray;
+
+@end
