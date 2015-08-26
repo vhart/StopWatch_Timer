088b8ac14a8d38d1f3fabee068b318804ae20f8f
Varindra <varindrahart@gmail.com>
2015-08-26 12:41:33 -0400

    pre pull

---
+++ Stopwatch_Timer/Stopwatch_Timer/LabelAnimator.h
@@ -0,0 +1,33 @@
+//
+//  LabelAnimator.h
+//  Stopwatch_Timer
+//
+//  Created by Varindra Hart on 8/25/15.
+//  Copyright (c) 2015 Varindra Hart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//RGB color macro
+#define UIColorFromRGB(rgbValue) [UIColor \
+colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \
+blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+//RGB color macro with alpha
+#define UIColorFromRGBWithAlpha(rgbValue,a) [UIColor \
+colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \
+blue:((float)(rgbValue & 0xFF))/255.0 alpha:a]
+
+@interface LabelAnimator : NSObject
+
+@property (nonatomic) UILabel * smallLabel;
+@property (nonatomic) UILabel * mediumLabel;
+@property (nonatomic) UILabel * largeLabel;
+
+- (instancetype)
+
+
+@end
---
+++ Stopwatch_Timer/Stopwatch_Timer/LabelAnimator.m
@@ -0,0 +1,17 @@
+//
+//  LabelAnimator.m
+//  Stopwatch_Timer
+//
+//  Created by Varindra Hart on 8/25/15.
+//  Copyright (c) 2015 Varindra Hart. All rights reserved.
+//
+
+#import "LabelAnimator.h"
+#import <UIKit/UIKit.h>
+
+
+@implementation LabelAnimator
+
+
+
+@end
